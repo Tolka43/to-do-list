31164f08b327a1f0afa094b331e7fb23f051ed4f
Ola Tomczyk <ola443@op.pl>
2020-11-04 11:31:11 +0100

    js-code

--- script.js
+++ script.js
@@ -1,19 +1,40 @@
 const taskInput = document.querySelector(".task-input");
 const addButton = document.querySelector(".add-button");
 const tasksDiv = document.querySelector(".tasks");
+const body = document.querySelector("body");
 
-const addTask = () => {
+const createTask = () => {
   const taskDiv = document.createElement("div");
-  const bin = document.createElement('i')
-  
+
   taskDiv.classList.add("task-div");
-  bin.classList.add('fas', 'fa-trash')
 
   taskDiv.innerHTML = taskInput.value;
   taskInput.value = "";
 
-  taskDiv.appendChild(bin)
-  tasksDiv.appendChild(taskDiv);
+  function tasksAction() {
+    taskDiv.style.textDecoration = "line-through";
+    taskDiv.style.color = "grey";
+  }
+
+  taskDiv.addEventListener("click", tasksAction);
+
+  return taskDiv;
+};
+
+const addTask = () => {
+  if (taskInput.value) {
+    tasksDiv.appendChild(createTask());
+  } else {
+    alert("Pole musi zostać wypełnione tekstem");
+  }
 };
 
 addButton.addEventListener("click", addTask);
+
+const enterKeydownListener = (event) =>{
+  if (event.key === "Enter") {
+    return addTask();
+  }
+};
+
+body.addEventListener("keydown", enterKeydownListener);
